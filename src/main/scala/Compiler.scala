0deeab1e24e35dbd7e70a00727966e7b58723c53
KolbeML <vplayer92@gmail.com>
2017-11-12 23:40:12 -0500

    Constants, other minor updates
    Added constants files and some other updates

--- src/main/scala/Compiler.scala
+++ src/main/scala/Compiler.scala
@@ -1,42 +1,59 @@
-
+import java.awt.Desktop
+import java.io._
 
 object Compiler{
   //variable declarations
   var currentT : String = ""
   var wholeFile : String = ""
+  var fileArr : Array[Char] = Array()
 
   val lexer = new Lexer
-  val syner = new MySyntaxAnalyzer
+  val syner = new Syner
   val semer = new MySemanticAnalyzer
   var atEnd : Boolean = false
-  var lineNum = 0 //count # lines for debug/error message purposes
+  var lineNum : Int = 0 //count # lines for debug/error message purposes
+  var tokens = List()
 
-  var filename: String = ""
+  var pos : Int = -1
+  var size : Int = 0
+  var count : Int = 0
 
 
   def currentT(t : String) = {
 
   }
 
-  def main(args : Array[String]) = {
-    filename = args(0)
-
+  def main(args: Array[String]): Unit = {
     checkUserInput(args)
     getText(args(0))
+    fileArr = wholeFile.toCharArray
+    lexer.getNextToken()
 
-    lexer.start(wholeFile)
-
-    //loops through the file
-    while (lexer.filePosition < lexer.filesize && !atEnd) {
-      lexer.getNextToken()
-      syner.gittex()
-
-      if (currentT.equalsIgnoreCase("\\END")) {
-        atEnd = true
-      }
-    }
-    semer.symantics()
   }
+
+//  def main(args : Array[String]) = {
+//    lexer.nextC = 0
+//
+//    while(fileArr(lexer.nextC + 1) != Nil) {
+//      lexer.getNextToken()
+//      lexer.tempT :: tokens
+//    }
+//    lexer.gittex()
+//
+//    semer.initStack()
+//    semer.variableRes()
+//
+//    val output = new File(filename + ".html")
+//
+//    val bw = new BufferedWriter(new FileWriter(output))
+//    while(semer.convertStack(count) != Nil) {
+//      bw.write(semer.convertStack(count))
+//      count += 1
+//    }
+//    bw.close()
+//
+//    openHTMLFileInBrowser(args(0))
+//  }
 
   //read the file into one giant string
   def getText(f : String) = {
@@ -62,6 +79,22 @@ object Compiler{
     if (!args(0).endsWith(".gtx")) {
       println("Error: Input file must be of filetype .gtx")
       System.exit(1)
+    }
+  }
+
+  /* * Hack Scala/Java function to take a String filename and open in default web browswer. */
+  def openHTMLFileInBrowser(htmlFileStr : String) = {
+    val file : File = new File(htmlFileStr.trim)
+    println(file.getAbsolutePath)
+    if (!file.exists())
+      sys.error("File " + htmlFileStr + " does not exist.")
+
+    try {
+      Desktop.getDesktop.browse(file.toURI)
+    }
+    catch {
+      case ioe: IOException => sys.error("Failed to open file:  " + htmlFileStr)
+      case e: Exception => sys.error("He's dead, Jim!")
     }
   }
 
--- src/main/scala/Syner.scala
+++ src/main/scala/Syner.scala
@@ -1,60 +1,40 @@
 class Syner extends SyntaxAnalyzer {
-  def gittex() : Unit = {
+    override def gittex(): Unit = {
+      if (Compiler.currentToken.equalsIgnoreCase(CONSTANTS.DOCB)){
+        // add to parse tree / stack
+        Compiler.Scanner.getNextToken()
+      }
+      else {
+        println("Error")
+        System.exit(1)
+      }
+    }
 
+    override def paragraph(): Unit = ???
+
+    override def innerItem(): Unit = ???
+
+    override def innerText(): Unit = ???
+
+    override def link(): Unit = ???
+
+    override def italics(): Unit = ???
+
+    override def body(): Unit = ???
+
+    override def bold(): Unit = ???
+
+    override def newline(): Unit = ???
+
+    override def title(): Unit = ???
+
+    override def variableDefine(): Unit = ???
+
+    override def image(): Unit = ???
+
+    override def variableUse(): Unit = ???
+
+    override def heading(): Unit = ???
+
+    override def listItem(): Unit = ???
   }
-
-
-  def title() : Unit = {
-
-  }
-
-
-  def body() : Unit = {
-
-  }
-
-
-  def paragraph() : Unit = {
-
-  }
-
-
-  def heading() : Unit = {
-
-  }
-
-
-  def variableDefine() : Unit = {
-
-  }
-
-
-  def variableUse() : Unit = {
-
-  }
-
-
-  def bold() : Unit = {
-
-  }
-
-
-  def listItem() : Unit = {
-
-  }
-
-
-  def link() : Unit = {
-
-  }
-
-
-  def image() : Unit = {
-
-  }
-
-
-  def newline() : Unit = {
-
-  }
-}
